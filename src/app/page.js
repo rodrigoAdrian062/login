63df9248326e6e3aa7e2ee979402654fb09f66e3
rodrigo <rodrigoadrian062@hotmail.com>
2025-01-18 18:35:27 -0300

    login

--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,58 @@
+import { Button } from "@/components/ui/button";
+import { FcGoogle } from "react-icons/fc";
 import Image from "next/image";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <main className="bg-[#1a191a] h-screen flex items-center justify-center p-10">
+      <div className="grid bg-center grid-cols-1 md:grid-cols-2">
         <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+          className=" max-lg:rounded-none max-md:rounded-full max-md:mb-7 max-md:w-44 max-md:m-auto "
+          src="/logo.jpg"
+          alt="Logo"
+          width={350}
+          height={0}
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+        <div className="bg-[#16202a]  text-white flex items-center justify-center flex-col max-md:p-5">
+          <div className="my-4">
+            <h1 className="flex justify-center text-3xl font-semibold">
+              Login
+            </h1>
+            <p className=" mt-2 text-xs text-slate-400">
+              lorem impsn lorem lorem pssdpp
+            </p>
+          </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+          <form>
+            <Button
+              className=" flex items-center w-full gap-4 mb-2 px-12 bg-transparent "
+              variant="outline"
+            >
+              <FcGoogle />
+              Logar com sua conta google
+            </Button>
+            <Label htmlFor="email">Email*</Label>
+            <Input
+              className="mb-2"
+              type="email"
+              id="email"
+              placehouder="Email"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <Label htmlFor="senha">Senha*</Label>
+            <Input type="Password" id="senha" placehouder="Senha" />
+
+            <button type="submit" className="w-full mt-4 bg-[#5d3165]">
+              Login
+            </button>
+
+            <p className="mt-4 text-xs text-slate-200 flex  justify-center max-md:mb-3">
+              @2025 Todos direitos reservados
+            </p>
+          </form>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+    </main>
   );
 }
